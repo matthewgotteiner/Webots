0176d67a56befd23d7af1a44bd0884a63454f726
Stephen Just <stephenjust@gmail.com>
2021-02-20 10:30:25 -0800

    Publish simulation time in world pose (#37)
    * Publish simulation time in world pose
    
    * Review comment

--- RobotServer/controllers/http_robot/http_robot.py
+++ RobotServer/controllers/http_robot/http_robot.py
@@ -74,6 +74,8 @@ def put_motors():
     position = robot_node.getPosition()
     rotation = robot_node.getOrientation()
     yaw = math.degrees(math.atan2(rotation[0], rotation[1])) % 360
+    # simulation time is reported in seconds
+    time = robot.getTime()
     return json.dumps({
         "Sensors": list(itertools.chain(
             distance_sensor_values,
@@ -82,7 +84,8 @@ def put_motors():
         )),
         "WorldPose": {
             "Position": position,
-            "Yaw": yaw
+            "Yaw": yaw,
+            "Time": time
         }
     })
 
