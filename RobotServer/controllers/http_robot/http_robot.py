ccfbb1ba0003dff836a958be7c7a93c48004250f
Alex Schokking <aschokking@gmail.com>
2021-03-11 19:50:18 -0800

    Draw circle function (#45)

--- RobotServer/controllers/http_robot/http_robot.py
+++ RobotServer/controllers/http_robot/http_robot.py
@@ -166,6 +166,20 @@ def put_arrow():
     
     return 'OK'
 
+
+@app.route("/overlay/circle", methods=['PUT'])
+def put_circle():
+    request_data = request.json or {}
+    
+    name = request_data['name']
+    center = request_data['center']
+    radius = request_data['radius']
+    color_rgb = request_data.get('color', [0, 1, 1])
+
+    draw_circle(name=name, center=center, color_rgb=color_rgb, radius=radius)
+    
+    return 'OK'
+
 def build_device_map(robot):
     device_map = defaultdict(dict)
 
@@ -306,6 +320,55 @@ def draw_arrow(name: str, point_1: List[float], point_2: List[float], color_rgb:
     point_field.setMFVec3f(1, point_2)
     point_field.setMFVec3f(2, arrow_left)
     point_field.setMFVec3f(3, arrow_right)
+
+
+def draw_circle(name: str, center: List[float], color_rgb: List[float], radius: float, num_segments=20):
+    # Create node with name if it doesn't exist yet
+    node_name = f'CIRCLE_{name}'
+
+    if node_name not in line_map:
+        node = robot.getFromDef(node_name)
+        root_node = robot.getRoot();
+        root_children_field = root_node.getField("children");
+
+        template = f"DEF {node_name} " + """Shape {
+            appearance Appearance {
+                material Material {
+                    emissiveColor """ + " ".join(map(str, color_rgb)) + """
+                }
+            }
+            geometry DEF TRAIL_LINE_SET IndexedLineSet {
+                coord Coordinate {
+                    point [
+                        """ + ("0 0 0\n" * num_segments) + """
+                    ]
+                }
+                coordIndex [
+                    """ + " ".join(map(str, range(num_segments))) + """ 0 -1
+                ]
+            }
+        }"""
+        root_children_field.importMFNodeFromString(-1, template)
+        node = robot.getFromDef(node_name)
+
+        # Update the coords based on points
+        trail_set_node = robot.getFromDef(f"{node_name}.TRAIL_LINE_SET")
+        coordinates_node = trail_set_node.getField("coord").getSFNode()
+        point_field = coordinates_node.getField("point")
+        line_map[node_name] = point_field
+    else:
+        point_field = line_map[node_name]
+
+    # Use polar coordinates to find points on the circle
+    for i in range(num_segments):
+        theta = math.pi * 2 / num_segments * i
+        point = [
+            center[0] + radius * math.cos(theta),
+            center[1] + radius * math.sin(theta),
+            center[2],
+        ]
+        point_field.setMFVec3f(i, point)
+    
 
 
 def start_flask():
