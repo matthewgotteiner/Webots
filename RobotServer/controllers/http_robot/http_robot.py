4d8a8dad11ca944228b41ab6dd35bf57a2df8954
Alex Schokking <aschokking@gmail.com>
2021-03-11 18:57:57 -0800

    draw_arrow() (#44)

--- RobotServer/controllers/http_robot/http_robot.py
+++ RobotServer/controllers/http_robot/http_robot.py
@@ -153,6 +153,19 @@ def put_line():
     
     return 'OK'
 
+@app.route("/overlay/arrow", methods=['PUT'])
+def put_arrow():
+    request_data = request.json or {}
+    
+    name = request_data['name']
+    point_1 = request_data['point_1']
+    point_2 = request_data['point_2']
+    color_rgb = request_data.get('color', [0, 1, 1])
+
+    draw_arrow(name, point_1, point_2, color_rgb=color_rgb)
+    
+    return 'OK'
+
 def build_device_map(robot):
     device_map = defaultdict(dict)
 
@@ -229,6 +242,70 @@ def draw_line(name: str, point_1: List[float], point_2: List[float], color_rgb: 
 
     point_field.setMFVec3f(0, point_1)
     point_field.setMFVec3f(1, point_2)
+
+
+# Draws a line between point_1 and point_2
+# name creates the node in the tree so we just update the same node each time
+def draw_arrow(name: str, point_1: List[float], point_2: List[float], color_rgb: List[float], arrow_angle_deg=35, arrow_len_ratio=0.25):
+    # Create node with name if it doesn't exist yet
+    node_name = f'ARROW_{name}'
+
+    if node_name not in line_map:
+        node = robot.getFromDef(node_name)
+        root_node = robot.getRoot();
+        root_children_field = root_node.getField("children");
+
+        template = f"DEF {node_name} " + """Shape {
+            appearance Appearance {
+                material Material {
+                    emissiveColor """ + " ".join(map(str, color_rgb)) + """
+                }
+            }
+            geometry DEF TRAIL_LINE_SET IndexedLineSet {
+                coord Coordinate {
+                    point [
+                        0 0 0
+                        0 0 0
+                        0 0 0
+                        0 0 0
+                    ]
+                }
+                coordIndex [
+                    0 1 -1
+                    1 2 -1
+                    1 3 -1
+                ]
+            }
+        }"""
+        root_children_field.importMFNodeFromString(-1, template)
+        node = robot.getFromDef(node_name)
+
+        # Update the coords based on points
+        trail_set_node = robot.getFromDef(f"{node_name}.TRAIL_LINE_SET")
+        coordinates_node = trail_set_node.getField("coord").getSFNode()
+        point_field = coordinates_node.getField("point")
+        line_map[node_name] = point_field
+    else:
+        point_field = line_map[node_name]
+
+    # calculate arrow points based on start/end
+    # convert to polar
+    delta = [point_1[0] - point_2[0], point_1[1] - point_2[1]]
+    length = math.sqrt(delta[0] ** 2 + delta[1] ** 2)
+    angle = math.atan2(delta[0], delta[1])
+
+    left_angle = angle + math.radians(arrow_angle_deg)
+    right_angle = angle - math.radians(arrow_angle_deg)
+
+    arrow_length = length * arrow_len_ratio
+    arrow_left = [arrow_length * math.sin(left_angle) + point_2[0], arrow_length * math.cos(left_angle) + point_2[1], point_2[2]]
+    arrow_right = [arrow_length * math.sin(right_angle) + point_2[0], arrow_length * math.cos(right_angle) + point_2[1], point_2[2]]
+
+
+    point_field.setMFVec3f(0, point_1)
+    point_field.setMFVec3f(1, point_2)
+    point_field.setMFVec3f(2, arrow_left)
+    point_field.setMFVec3f(3, arrow_right)
 
 
 def start_flask():
